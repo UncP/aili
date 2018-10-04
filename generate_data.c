628211db202892d5fd821db9047a328b0cc73aed
UncP <uncp.xu@gmail.com>
2018-10-04 18:12:41 +0800

    generate sequential test data & fix incomplete test data generation

--- generate_data.c
+++ generate_data.c
@@ -13,30 +13,10 @@
 #include <sys/stat.h>
 #include <time.h>
 
-int main(int argc, char **argv)
+static const char *dir = "./data";
+
+static void generate_test_data(const char *file_name, int key_num, int key_len, int random)
 {
-  if (argc < 3) {
-    printf("key_number key_length [file_name]\n");
-    exit(1);
-  }
-
-  int key_num = atoi(argv[1]);
-  int key_len = atoi(argv[2]);
-  char *file_name = "0\0";
-  if (argc == 4) file_name = argv[3];
-  if (key_num <= 0) key_num = 1000000;
-  if (key_num > 100000000) key_num = 100000000;
-  if (key_len <= 0) key_len = 16;
-  if (key_len > 255) key_len = 255;
-
-  const char *string = "abcdefghijklmnopqrstuvwxyz0123456789";
-  const int str_len = strlen(string);
-
-  const char *dir = "./data";
-
-  if (access(dir, F_OK))
-    assert(mkdir(dir, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IROTH) >= 0);
-
   srand(time(NULL));
 
   uint64_t total_len = 0;
@@ -48,12 +28,26 @@ int main(int argc, char **argv)
   int fd = open(file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP);
   assert(fd != -1);
 
+  const char *string = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const int str_len = strlen(string);
+
   char buf[8192];
   int ptr = 0;
+  char key[key_len + 1];
+  for (int i = 0; i < key_len; ++i)
+    key[i] = '0';
   for (int i = 0; i < key_num; ++i) {
-    char key[key_len + 1];
-    for (int k = 0; k < key_len; ++k)
-      key[k] = string[rand() % str_len];
+    if (random) {
+      for (int k = 0; k < key_len; ++k)
+        key[k] = string[rand() % str_len];
+    } else {
+      int cur = key_len - 1;
+      while (key[cur] + 1 > '9') {
+        key[cur] = '0';
+        assert(--cur >= 0);
+      }
+      ++key[cur];
+    }
     key[key_len] = '\n';
     if (ptr + key_len + 1 >= 8192) {
       assert(pwrite(fd, buf, ptr, total_len) == ptr);
@@ -66,9 +60,36 @@ int main(int argc, char **argv)
       printf("total %d, generated %d\n", key_num, i);
   }
 
-  fsync(fd);
-  close(fd);
-  printf("name:%s  key_number:%d  key_length:%d\n", file, key_num, key_len);
+  assert(pwrite(fd, buf, ptr, total_len) == ptr);
 
+  close(fd);
+}
+
+int main(int argc, char **argv)
+{
+  if (argc < 3) {
+    printf("key_number key_length\n");
+    exit(1);
+  }
+
+  int key_num = atoi(argv[1]);
+  int key_len = atoi(argv[2]);
+  char *file_name1 = "1\0";
+  char *file_name2 = "0\0";
+  if (key_num <= 0) key_num = 1000000;
+  if (key_num > 100000000) key_num = 100000000;
+  if (key_len <= 0) key_len = 16;
+  if (key_len > 255) key_len = 255;
+
+  if (access(dir, F_OK))
+    assert(mkdir(dir, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IROTH) >= 0);
+
+  printf("generate random test data\n");
+  generate_test_data(file_name1, key_num, key_len, 1 /* random */);
+
+  printf("generate sequential test data\n");
+  generate_test_data(file_name2, key_num, key_len, 0 /* random */);
+
+  printf("key_num: %d  key_len: %d\n", key_num, key_len);
   return 0;
 }
